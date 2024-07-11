91b0bd39ef4c47c2ceffae185c48462f40020edc
Dmitry <dmi_i88@mail.ru>
2024-07-11 21:47:25 +0700

    11.07.24

--- $/learnphptherightway-project-1.31/app/App.php
+++ $/learnphptherightway-project-1.31/app/App.php
@@ -27,10 +27,81 @@ fclose($file);
 */
 // $contStr = file_get_contents('../transaction_files/sample_1.csv');
 $contArr = file('../transaction_files/sample_1.csv');
-// var_dump($contArr);
+var_dump($contArr);
 // $chunks = explode(',', $contStr);
 // print_r ($chunks);
+// $data=[];
+
 foreach($contArr as $str) {
-   $transactions =  explode(',', $str);
+   $transactions =  explode(',', $str, 4);
    print_r ($transactions);
+   $data[] = $transactions[0];
+   $check_№[] = $transactions[1];
+   $description[] = $transactions[2];
+   $amount[] = $transactions[3];
+
+
+//    for(i = 0; i<count($transactions); i++){
+// ;
+//    };
 }
+
+
+print_r ($data);
+print_r ($check_№);
+print_r ($description);
+print_r ($amount);
+$income = 0;
+$expense = 0;
+echo "Дальше идет ФОРЫЧ " . "\n";
+
+foreach(array_slice($amount, 1) as $str){
+    echo $str ."\n";
+    var_dump ($str);
+    //echo $str[1] ."\n";
+    $noDollar = str_replace('$', '', $str);
+    $noQ = str_replace('"', '', $noDollar);
+
+    //$getFloat = (float)$noDollar;
+    echo $noQ ;
+    var_dump ($noQ);
+    // echo $getFloat ."\n";
+
+    // if($str[0]== '-'){
+    // echo "Без доллара  A равно " . $a."\n";
+   
+
+    //    // $noDollarExp = (float)$a;
+        
+    // }
+
+   // echo "Без доллара равно " . $noDollarExp;
+
+    // $num = (float)$str;
+    // var_dump ($num);
+
+    // if ($num > 0) {
+    //     $income += $num;
+    // } else {
+    //     $expense -= $num;
+    // }
+    // return $income;
+}
+
+$k = "-$150.43";
+$x = str_replace('"','$', '', $k);
+var_dump ($x);
+
+$t = (float)$x;
+var_dump ($t);
+var_dump ($k);
+//var_dump ($amount[0]);
+
+
+
+// echo $income."\n";
+// echo $expense."\n";
+
+
+
+
